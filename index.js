125aee7bd8beade329d5ab01d8e519f0ba14636b
Abdullah Al Noman <abdullahalnoman1512@gmail.com>
2021-04-05 23:34:12 +0600

    api created

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ app.use(cors())
 app.use(express.json())
 const MongoClient = require('mongodb').MongoClient;
 
-const uri= "mongodb+srv://emaJhon:test89@cluster0.ez7qy.mongodb.net/emaJhonSimple?retryWrites=true&w=majority"
+// const uri= "mongodb+srv://emaJhon:test89@cluster0.ez7qy.mongodb.net/emaJhonSimple?retryWrites=true&w=majority"
+const uri= `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ez7qy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
-
+console.log(process.env.DB_USER,process.env.DB_PASS,process.env.DB_NAME);
 
 client.connect(err => {
   const collection = client.db("emaJhonSimple").collection("products");
   const ordersCollection = client.db("emaJhonSimple").collection("orders");
-
 
   app.post('/adProducts',(req,res)=>{
         const products=req.body;
